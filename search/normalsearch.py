de2b44500d1293116528de570880a830be02b116
Abhigyan <abhigyanbhattacharya75@gmail.com>
2024-03-20 20:54:29 +0530

    update

--- search/normalsearch.py
+++ search/normalsearch.py
@@ -1,6 +1,11 @@
-list1 = [1,2,3,4,5,6,7,8,9]
-search = int (input("enter the number to be searched:"))
-for i in range (len(list1)):
-    if(list1[i]==search):
-        print("Yay!")
-        break
+list = [55,2,3,88,5,6,7,8,9]
+search = int(input("Enter number to search: "))
+def searchList(list1,search):
+    # search = int (input("enter the number to be searched:"))
+    for i in range (len(list1)):
+        if(list1[i]==search):
+            print("Yay!Found")
+            print("The element is in the index:",i+1)
+            break
+
+searchList(list,search)
--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -1,5 +1,5 @@
-list1 = [8,2,3,6,5,7]
-for i in range (1,len(list1)):
+list1 = list ( range(10000,0,-1))
+for i in range (len(list1)):
     j=i
     while j>0 and list1[j]<list1[j-1]:
         (list1[j],list1[j-1])=(list1[j-1],list1[j])
